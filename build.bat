59acc480cdfb645e4e47bdb2c618cb19ddc027e2
CannondaleF400 <jlbougie@hotmail.com>
2016-12-03 20:17:34 -0600

    build increment

--- build.bat
+++ build.bat
@@ -4,6 +4,12 @@ REM try again
 ECHO try try again
 ECHO try try try again
 ECHO try try try try again
+set /p ver=<version.txt
+echo %ver%
+set /a ver = %ver% + 1
+echo %ver%
+echo %ver% > version.txt
+
 if exist Products rd /s /q Products
 mkdir Products
-echo build > Products\build.s19
+echo build > Products\build%ver%.s19
